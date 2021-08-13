d6f7f4dc05861ae00343b75fabe301f31881bcb3
Diego Llopis <diego.llopis1@gmail.com>
2021-08-13 16:19:13 -0300

    Implementação teste unitário MKPLacemark extensions

---
+++ HeremindersTests/MKPlaceMarkExtensionsUnitTest.swift
@@ -0,0 +1,32 @@
+//
+//  MKPlaceMarkExtensionsUnitTest.swift
+//  HeremindersTests
+//
+//  Created by Diego Llopis on 13/08/21.
+//  Copyright © 2021 Rodrigo Borges. All rights reserved.
+//
+
+import XCTest
+import MapKit
+import Contacts
+@testable import Hereminders
+
+class MKPlaceMarkExtensionsUnitTest: XCTestCase {
+
+    func testExample() throws {
+        
+        let coords = CLLocationCoordinate2D(latitude: 51.5083, longitude: -0.1384)
+        
+        let address = [CNPostalAddressStreetKey: "Rua Bela Cintra, 1491", CNPostalAddressSubLocalityKey: "Consolação"]
+        
+        let placemark = MKPlacemark(coordinate: coords, addressDictionary: address)
+        
+        let addressTest = placemark.formattedAddress()
+        
+        print(addressTest)
+        
+        let correctAddress = "Rua Bela Cintra, 1491, Consolação"
+
+        XCTAssert(addressTest == correctAddress)
+    }
+}
