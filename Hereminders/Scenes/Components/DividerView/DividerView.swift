14b605844cf835515f2f45c778e25f561604f7f4
Diego Llopis <diego.llopis1@gmail.com>
2021-08-20 08:25:39 -0300

    DividerView e respectivo teste de snapshot

---
+++ Hereminders/Scenes/Components/DividerView/DividerView.swift
@@ -0,0 +1,31 @@
+//
+//  DividerView.swift
+//  Hereminders
+//
+//  Created by Diego Llopis on 19/08/21.
+//  Copyright © 2021 Rodrigo Borges. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class DividerView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        self.configureSubviews()
+        self.configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureSubviews() {
+        self.backgroundColor = .lightGray
+    }
+    
+    private func configureConstraints() {
+        self.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
+    }
+}
